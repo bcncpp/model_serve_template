38272eb3e2e3c0eb5661db6376b92b063e2bf9b4
Giorgio Zoppi <sba23101@student.cct.ie>
2025-06-19 23:07:53 +0100

    test all

--- model_inference/main.py
+++ model_inference/main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-from api.routes import users
-
+from api.routes import sensor_anomalies
 app = FastAPI()
-
-app.include_router(users.router, prefix="/api")
+app.include_router(sensor_anomalies.router, prefix="/api")
